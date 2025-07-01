0b469bd22b910ffb196c7fd181bb288b7ef0ca4d
elieiradukunda <iradukundaelie71@gmail.com>
2025-07-01 19:01:32 +0200

    updated role api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 // backend/routes/userRoutes.js
 import express from 'express';
 import { register, login } from '../controllers/userController.js';
+import { getall,getUserById,updateUser,deleteUser } from '../models/userModel.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/all',getall)
+
+router.get("/:id", getUserById);     
+router.put("/:id", updateUser);      
+router.delete("/:id", deleteUser);  
+
+
 
 export default router;
